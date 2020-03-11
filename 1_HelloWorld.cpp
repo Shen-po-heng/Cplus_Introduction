9184baace6102ad614039811a53a5584d01643fb
ShenPaul <shenpaulheng@gmail.com>
2020-03-11 18:42:09 +0800

    Add files via upload

---
+++ 1_HelloWorld.cpp
@@ -0,0 +1,11 @@
+/*
+File name: 1st practice of C++
+Date:11.03.2020
+Editor: Shen Paul
+Content: Build the first file of C++ and try to print out "Hello World" on command line. 
+*/ 
+#include<iostream>
+using namespace std;
+int main(void){
+	cout<<"Hello World!!";
+} 
